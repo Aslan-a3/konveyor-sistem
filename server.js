9f3366b8fe04100776b3098bb9c964f9d083d46e
Muhammed <blankinfinity58@gmail.com>
2026-05-16 01:47:36 +0300

    fix port

--- server.js
+++ server.js
@@ -66,4 +66,4 @@ app.get('/api/istatistik', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log('Railway sunucu hazır: ' + PORT));
+server.listen(PORT, '0.0.0.0', () => console.log('Railway sunucu hazır: ' + PORT));
